b7fdca855696b37e3780d592e807517d2f3f0e9b
Maruan256 <maruanpaschen@gmail.com>
2025-02-03 15:10:23 +0100

    footer centered

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
         <>
-        <div className="h-10 sticky bottom-0">
+        <div className="h-10 bottom-0 flex justify-center items-center">
             <p>Design und Code by © Fritz von Buchholz</p>
         </div>
         </>
